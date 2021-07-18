f5a494acf5fe0edfdc8a81f22921f8f385dc7f9a
Lucye <lethiciass.r@gmail.com>
2021-07-18 11:38:58 -0500

    Inicio de proyecto cloud

---
+++ PI_GitCloudBasics/src/main/java/com/nomadadigital/git/PiGitCloudBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.nomadadigital.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiGitCloudBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiGitCloudBasicsApplication.class, args);
+	}
+
+}
---
+++ PI_GitCloudBasics/src/test/java/com/nomadadigital/git/PiGitCloudBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomadadigital.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiGitCloudBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
